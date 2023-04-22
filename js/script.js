fa21373801e19119207c6f801481fff44da91ed9
Murasakino <120464393+FireDragonGod@users.noreply.github.com>
2023-04-22 13:21:14 +0800

    Create a func that operates on the above func

--- js/script.js
+++ js/script.js
@@ -9,4 +9,30 @@ let number = '';
 let operator = '';
 let anotherNumber = '';
 
-
+const operate = (operations, num1, num2) => {
+  let sum = 0;
+  let numOne = +num1;
+  let numTwo = +num2;
+  if (operations === '' && num1 === '' || num2 === '') {
+    return sum = '';
+  }
+  if (operations.slice(-1) === '➗' && numOne === 0 && numTwo === 0) {
+    return sum = alert(undefined),
+      number = '',
+      anotherNumber = '',
+      operator = '';
+  }
+  if (operator.slice(-1) === '+') {
+    sum += add(numOne, numTwo);
+  }
+  if (operations.slice(-1) === '-') {
+    sum += subtract(numOne, numTwo);
+  }
+  if (operations.slice(-1) === 'x') {
+    sum += multiply(numOne, numTwo);
+  }
+  if (operations.slice(-1) === '➗') {
+    sum += divide(numOne, numTwo);
+  }
+  return Math.round(sum * 10000000000) / 10000000000;
+};
