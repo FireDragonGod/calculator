5b8c1b10d61faca0ffcf99b999032786f045d643
Murasakino <120464393+FireDragonGod@users.noreply.github.com>
2023-04-22 13:18:51 +0800

    Declare three variables for num1 operator and num2

--- js/script.js
+++ js/script.js
@@ -5,5 +5,8 @@ const multiply = (num1, num2) => num1 * num2;
 const subtract = (num1, num2) => num1 - num2;
 const add = (num1, num2) => num1 + num2;
 
+let number = '';
+let operator = '';
+let anotherNumber = '';
 
 
